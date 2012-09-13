75192fb6075c5213c690487aeadbf9fe14a2d7da
Israel Buitron <israel.buitron@gmail.com>
2012-09-13 15:01:52 -0500

    PreSignup Activity added.
    Notice:
    - Methods for button actions in view are not implemented yet.
    
    Signed-off-by: Israel Buitron <israel.buitron@gmail.com>

---
+++ src/mx/ferreyra/dogapp/PreSignup.java
@@ -0,0 +1,26 @@
+package mx.ferreyra.dogapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class PreSignup extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pre_signup);
+    }
+    
+    public void onClickSignupButton(View view) {
+    	// TODO implement this method
+    }
+    
+    public void onClickLoginButton(View view) {
+    	// TODO implement this method
+    }
+    
+    public void onClickRecoverPasswordButton(View view) {
+    	// TODO implement this method
+    }
+}

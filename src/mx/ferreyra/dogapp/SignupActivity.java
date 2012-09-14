4b2e455913ea240fa2982b86e1e6a5021e58fde7
Israel Buitron <israel.buitron@gmail.com>
2012-09-13 22:27:41 -0500

    'Signup' layout was improved.
    Signed-off-by: Israel Buitron <israel.buitron@gmail.com>

--- src/mx/ferreyra/dogapp/SignupActivity.java
+++ src/mx/ferreyra/dogapp/SignupActivity.java
@@ -2,6 +2,7 @@ package mx.ferreyra.dogapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignupActivity extends Activity {
 
@@ -9,5 +10,13 @@ public class SignupActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+    public void onClickSignup(View view) {
+        // TODO implement this method
+    }
+
+    public void onClickSignupWithFacebook(View view) {
+        // TODO implement this method
     }
 }

0f8c07748e5fd5934a91d1d1ac5e69dbb2c20a5c
Israel Buitron <israel.buitron@gmail.com>
2012-09-14 01:04:29 -0500

    'Login' activity scaffold was generated.
    Notice:
    - Methods for button actions in view are not implemented yet.
    
    Signed-off-by: Israel Buitron <israel.buitron@gmail.com>

---
+++ src/mx/ferreyra/dogapp/LoginActivity.java
@@ -0,0 +1,22 @@
+package mx.ferreyra.dogapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void onClickLogin(View view) {
+        // TODO implement this method
+    }
+
+    public void onClickLoginWithFacebook(View view) {
+        // TODO implement this method
+    }
+}

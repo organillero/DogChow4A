7318f7e7ca1320b6383abc7c3668f0d39800bc85
Israel Buitron <israel.buitron@gmail.com>
2012-09-13 21:48:50 -0500

    'Signup' activity scaffold was generated.
    Signed-off-by: Israel Buitron <israel.buitron@gmail.com>

---
+++ src/mx/ferreyra/dogapp/SignupActivity.java
@@ -0,0 +1,13 @@
+package mx.ferreyra.dogapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SignupActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}

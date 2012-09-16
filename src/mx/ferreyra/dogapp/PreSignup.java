536b88e34c0dd8977ff0aaf1e9dc2c3b2d381026
Israel Buitron <israel.buitron@gmail.com>
2012-09-16 16:38:07 -0500

    Actions for buttons in 'PreSignup' activity were added.
    Signed-off-by: Israel Buitron <israel.buitron@gmail.com>

--- src/mx/ferreyra/dogapp/PreSignup.java
+++ src/mx/ferreyra/dogapp/PreSignup.java
@@ -1,6 +1,7 @@
 package mx.ferreyra.dogapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -11,16 +12,16 @@ public class PreSignup extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pre_signup);
     }
-    
+
     public void onClickSignupButton(View view) {
-    	// TODO implement this method
+        startActivity(new Intent(this, SignupActivity.class));
     }
-    
+
     public void onClickLoginButton(View view) {
-    	// TODO implement this method
+        startActivity(new Intent(this, LoginActivity.class));
     }
-    
+
     public void onClickRecoverPasswordButton(View view) {
-    	// TODO implement this method
+        startActivity(new Intent(this, RetrievePassword.class));
     }
 }

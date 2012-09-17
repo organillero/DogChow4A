205d1c890b92b78608648f4ba47001015f715c8d
Israel Buitron <israel.buitron@gmail.com>
2012-09-17 00:45:18 -0500

    Loading preferences added to application.
    Signed-off-by: Israel Buitron <israel.buitron@gmail.com>

--- src/mx/ferreyra/dogapp/DogChowApplication.java
+++ src/mx/ferreyra/dogapp/DogChowApplication.java
@@ -1,20 +1,34 @@
 package mx.ferreyra.dogapp;
 
 import android.app.Application;
+import android.content.SharedPreferences;
 
 /**
  * @author Israel Buitron
  */
 public class DogChowApplication extends Application {
+    private Integer currentUserId;
     private static DogChowApplication app;
 
     @Override
     public void onCreate() {
         super.onCreate();
         DogChowApplication.app = this;
+
+        // Loading preferences
+        String prefer = getString(R.string.preferences_name);
+        String userId = getString(R.string.preference_user_id);
+        SharedPreferences pref = getSharedPreferences(prefer, 0);
+        userId = pref.getString(userId, null);
+        if(userId!=null)
+            this.currentUserId = Integer.valueOf(userId);
     }
 
     public static DogChowApplication getInstance() {
         return app;
+    }
+
+    public Integer getCurrentUserId() {
+        return this.currentUserId;
     }
 }

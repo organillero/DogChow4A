98dd316ef01467809ec5b616a1092fb3561b00cb
Carlos Ferreyra <cferreyra@pagatodo.com>
2012-09-17 03:47:41 -0500

    merge

---
+++ src/mx/ferreyra/dogmap/UserInfoFB.java
@@ -0,0 +1,16 @@
+package mx.ferreyra.dogmap;
+
+import java.io.Serializable;
+
+public class UserInfoFB implements Serializable {
+
+	private static final long serialVersionUID = 786686958303202306L;
+
+	public String id;
+	public String first_name;
+	public String last_name;
+	public String email;
+	public String birthday;
+	public String gender;
+
+}

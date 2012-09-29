fdbe92b63895a7c1f4df2a1f8c297e176caadd25
Israel Buitron <israel.buitron@gmail.com>
2012-09-29 10:43:15 -0500

    Scaffold methods for buttons actions in AddDogPhoto activity added.
    Signed-off-by: Israel Buitron <israel.buitron@gmail.com>

--- src/mx/ferreyra/dogapp/AddDogPhoto.java
+++ src/mx/ferreyra/dogapp/AddDogPhoto.java
@@ -2,6 +2,7 @@ package mx.ferreyra.dogapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddDogPhoto extends Activity {
 
@@ -9,5 +10,17 @@ public class AddDogPhoto extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_dog_photo);
+    }
+
+    public void onClickDogPhotoButton(View view) {
+        // TODO implement this method
+    }
+
+    public void onClickRemoveDogPhotoButton(View view) {
+        // TODO implement this method
+    }
+
+    public void onClickAddPhotoButton(View view) {
+        // TODO implement this method
     }
 }

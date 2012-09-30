d62de3eadcf21d74a14c28226bb8712b13a25d9c
Israel Buitron <israel.buitron@gmail.com>
2012-09-29 23:35:55 -0500

    Bean added to map 'getFotosMascotaByLatLon' method response.
    Signed-off-by: Israel Buitron <israel.buitron@gmail.com>

--- src/mx/ferreyra/dogapp/WsDogUtils.java
+++ src/mx/ferreyra/dogapp/WsDogUtils.java
@@ -848,4 +848,88 @@ public class WsDogUtils {
 
         return envelope.bodyIn;
     }
+
+    public class FotosMascotaByLatLonResponse {
+        private Long photoId;
+        private Long ownerId;
+        private Date date;
+        private String photoAsBase64Binary;
+        private Double latitude;
+        private Double longitude;
+        private String comments1;
+        private String comments2;
+        private Double distance;
+
+        public Long getPhotoId() {
+            return photoId;
+        }
+
+        public void setPhotoId(Long photoId) {
+            this.photoId = photoId;
+        }
+
+        public Long getOwnerId() {
+            return ownerId;
+        }
+
+        public void setOwnerId(Long ownerId) {
+            this.ownerId = ownerId;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getPhotoAsBase64Binary() {
+            return photoAsBase64Binary;
+        }
+
+        public void setPhotoAsBase64Binary(String photoAsBase64Binary) {
+            this.photoAsBase64Binary = photoAsBase64Binary;
+        }
+
+        public Double getLatitude() {
+            return latitude;
+        }
+
+        public void setLatitude(Double latitude) {
+            this.latitude = latitude;
+        }
+
+        public Double getLongitude() {
+            return longitude;
+        }
+
+        public void setLongitude(Double longitude) {
+            this.longitude = longitude;
+        }
+
+        public String getComments1() {
+            return comments1;
+        }
+
+        public void setComments1(String comments1) {
+            this.comments1 = comments1;
+        }
+
+        public String getComments2() {
+            return comments2;
+        }
+
+        public void setComments2(String comments2) {
+            this.comments2 = comments2;
+        }
+
+        public Double getDistance() {
+            return distance;
+        }
+
+        public void setDistance(Double distance) {
+            this.distance = distance;
+        }
+    }
 }
